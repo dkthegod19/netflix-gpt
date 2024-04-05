909318e03bbfb7a471d4740bbb7f856d3d2ba2b6
Divyam Kumar <dkthegod19@gmail.com>
2024-04-05 20:06:02 +0530

    Built Main Container

--- components/Login.js
+++ components/Login.js
@@ -7,12 +7,14 @@ import {
   updateProfile,
 } from "firebase/auth";
 import { auth } from "../utils/firebase";
-import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { BG_URL, USER_AVATAR } from "../utils/constants";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
-  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const name = useRef(null);
   const email = useRef(null);
@@ -34,10 +36,18 @@ const Login = () => {
           const user = userCredential.user;
           updateProfile(user, {
             displayName: name.current.value,
-            photoURL: "https://avatars.githubusercontent.com/u/12824231?v=4",
+            photoURL: USER_AVATAR,
           })
             .then(() => {
-              navigate("/browse");
+              const { uid, email, displayName, photoURL } = auth.currentUser;
+              dispatch(
+                addUser({
+                  uid: uid,
+                  email: email,
+                  displayName: displayName,
+                  photoURL: photoURL,
+                })
+              );
             })
             .catch((error) => {
               setErrorMessage(error.message);
@@ -58,8 +68,6 @@ const Login = () => {
         .then((userCredential) => {
           // Signed in
           const user = userCredential.user;
-          console.log(user);
-          navigate("/browse");
         })
         .catch((error) => {
           const errorCode = error.code;
@@ -71,10 +79,16 @@ const Login = () => {
 
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
-      <div>    
+  };
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img className="h-screen object-cover" src={BG_URL} alt="logo" />
+      </div>
       <form
         onSubmit={(e) => e.preventDefault()}
-        className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80"
+        className="w-full md:w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80"
       >
         <h1 className="font-bold text-3xl py-4">
           {isSignInForm ? "Sign In" : "Sign Up"}
@@ -114,6 +128,6 @@ const Login = () => {
         </p>
       </form>
     </div>
-  };
+  );
 };
 export default Login;
---
+++ components/SecondaryContainer.js
@@ -0,0 +1,7 @@
+import { useSelector } from "react-redux";
+import MovieList from "./MovieList";
+
+const SecondaryContainer = () => {
+    return <div>SecondaryContainer</div>;
+  };
+  export default SecondaryContainer;
